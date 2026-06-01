f76ed451a5293c6c3bf7b6cb689befbae4a509ca
Pranav <pranavktrec@gmail.com>
2026-06-01 17:11:33 +0530

    Add intentional vulnerabilities for testing - eval(), XSS, hardcoded credentials, insecure dependencies, and more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
+// VULNERABILITY: No CSRF protection configured
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
